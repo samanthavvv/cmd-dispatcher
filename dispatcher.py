0b41056087aba7646189cae88b9a584516709ff7
samanthavvv <18621599514@163.com>
2022-06-30 11:28:23 +0800

    dispatcher 中添加 main 单元测试代码

--- dispatcher.py
+++ dispatcher.py
@@ -1,15 +1,26 @@
+def addfn(x=1,y=2):
+    return x+y
+
 class Dispatcher:
-    cmds = {}
+    CMDS = {}
 
     def reg(self, cmd, fn):
-        cmds[cmd] = fn
+        if cmd == 'add':
+            self.CMDS[cmd] = addfn
 
     def run(self):
         while True:
             cmd = input('plz input your command ').strip()
             if cmd == 'quit':
                 return
-            self.cmds.get(cmd, self.defaultfn)()
+            self.CMDS.get(cmd, self.defaultfn)()
 
     def defaultfn(self):
         print('unknow command')
+
+
+if __name__ == '__main__':
+    dis = Dispatcher()
+    dis.run()
+    print(dis.CMDS)
+    print(dis.__dict__)
